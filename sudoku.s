4fee57315e5c06381fb5a2164b4da6e8bc2bee57
Adrian.Conlon <adrian.conlon@gmail.com>
2016-04-28 16:39:00 +0100

    Start writing some of the algorithm support routines. Not tested!
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- sudoku.s
+++ sudoku.s
@@ -1,10 +1,21 @@
+//
+// From: https://see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf
+//
+// A straightforward port from C to 6502 assembler
+//
+// http://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
+
         .setcpu "6502"
 
         .segment "OS"
 
 .include "library.inc"
 
+; Scratchpad area.  Not sure how long this will be.  Aliased within "proc" scope.
+
 scratch := 80
+
+; Some useful constants
 
 UNASSIGNED := 0
 BOX_SIZE := 3
@@ -22,13 +33,9 @@ puzzle:
 	.byte 0, 0, 8, 5, 0, 0, 0, 1, 0
 	.byte 0, 9, 0, 0, 0, 0, 4, 0, 0
 
-;int move2x(int n) {
-;	return n % BOARD_SIZE;
-;}
-
-;int move2y(int n) {
-;	return n / BOARD_SIZE;
-;}
+;
+; ** Move and grid position translation methods
+;
 
 .proc move2xy
 	sta library::maths::numerator
@@ -40,9 +47,6 @@ puzzle:
 	rts
 .endproc
 
-;int xy2move(int x, int y) {
-;	return y * BOARD_SIZE + x;
-;}
 
 .proc xy2move
 	sty library::maths::first
@@ -56,12 +60,14 @@ puzzle:
 	rts
 .endproc
 
-;int move2row_start(int n) {
-;	return move2y(n) * BOARD_SIZE;
-;}
+
+; ** Row, column and box start positions
 
 .proc move2row_start
-	sta scratch
+
+_n := scratch
+
+	sta _n
 	txa
 	pha
 	tya
@@ -76,16 +82,16 @@ puzzle:
 	tay
 	pla
 	tax
-	lda scratch
+	lda _n
 	rts
 .endproc
 
-;int move2column_start(int n) {
-;	return move2x(n);
-;}
 
 .proc move2column_start
-	sta scratch
+
+_n := scratch
+
+	sta _n
 	txa
 	pha
 	tya
@@ -95,21 +101,237 @@ puzzle:
 	tay
 	pla
 	tax
-	lda scratch
+	lda _n
 	rts
 .endproc
 
-;int box_side_start(int n) {
-;	return n - (n % BOX_SIZE);
-;}
 
-;int move2box_start(int n) {
-;	int x = move2x(n);
-;	int xbox = box_side_start(x);
-;	int y = move2y(n);
-;	int ybox = box_side_start(y);
-;	return xy2move(xbox, ybox);
-;}
+.proc box_side_start
+
+_n := scratch
+
+	sta _n
+
+	txa
+	pha
+
+	sta library::maths::numerator
+
+	lda #BOX_SIZE
+	sta library::maths::denominator
+	jsr library::maths::divmod
+
+	tax
+	lda scratch
+	stx scratch
+
+	clc
+	sbc scratch
+	sta scratch
+
+	pla
+	tax
+
+	lda scratch
+
+	rts
+.endproc
+
+
+.proc move2box_start
+
+_x := scratch
+_y := _x + 1
+_xbox := _y + 1
+_ybox := _xbox + 1
+_n := _ybox + 1
+
+	sta _n
+
+	txa
+	pha
+
+	tya
+	pha
+
+	jsr move2xy
+	stx _x
+	sty _y
+
+	txa
+	jsr box_side_start
+	sta _xbox
+
+	tya
+	jsr box_side_start
+	sta _ybox
+
+	ldx _xbox
+	ldy _ybox
+	jsr xy2move
+	sta _n
+
+	pla
+	tay
+
+	pla
+	tax
+
+	lda _n
+
+	rts
+.endproc
+
+
+; Function: is_used_in_row
+; ------------------------
+; Returns a boolean which indicates whether any assigned entry
+; in the specified row matches the given number.
+
+.proc is_used_in_row
+
+_number := scratch
+_n := _number + 1
+_savex := _n + 1
+
+_offset := _savex + 1
+
+	sta _number
+	sty _n
+	stx _savex
+	tya
+	jsr move2row_start
+	sta _offset
+	ldx _offset
+	ldy #BOARD_SIZE
+loop:
+	lda puzzle,x
+	cmp _number
+	beq success
+	inx
+	dey
+	bne loop
+	lda #1		; failure, zero cleared
+	pha
+	jmp continue
+success:
+	lda #0		; success, zero flag
+	pha
+continue:
+	ldx _savex
+	pla
+	rts
+.endproc
+
+
+; Function: is_used_in_column
+; ---------------------------
+; Returns a boolean which indicates whether any assigned entry
+; in the specified column matches the given number.
+
+.proc is_used_in_column
+
+_number := scratch
+_n := _number + 1
+_savex := _n + 1
+
+_offset := _savex + 1
+
+	sta _number
+	sty _n
+	stx _savex
+	tya
+	jsr move2column_start
+	sta _offset
+	ldx _offset
+	ldy #BOARD_SIZE
+loop:
+	lda puzzle,x
+	cmp _number
+	beq success
+
+	txa
+	clc
+	adc #BOARD_SIZE
+	tax
+
+	dey
+	bne loop
+	lda #1		; failure, zero cleared
+	pha
+	jmp continue
+success:
+	lda #0		; success, zero flag
+	pha
+continue:
+	ldx _savex
+	pla
+	rts
+.endproc
+
+
+; Function: is_used_in_box
+; ------------------------
+; Returns a boolean which indicates whether any assigned entry
+; within the specified 3x3 box matches the given number.
+
+.proc is_used_in_box
+
+_box_start := scratch
+_x_box := _box_start + 1
+_y_box := _x_box + 1
+_offset := _y_box + 1
+
+_number := _offset +1
+_n := _number + 1
+
+_savex := _n + 1
+
+	sta _number
+	sty _n
+	stx _savex
+	tya
+	jsr move2box_start
+	sta _box_start
+
+	ldy #BOARD_SIZE
+loop:
+	tya
+	pha
+
+	sty maths::numerator
+	lda #BOX_SIZE
+	sta maths::denominator
+	jsr maths::divmod
+
+	jsr xy2move
+	clc
+	adc _box_start
+	tax
+
+	lda puzzle,x
+	cmp _number
+	beq success
+
+	pla
+	tay
+
+	dey
+	beq loop
+
+	lda #1		; failure, zero cleared
+	pha
+	jmp continue
+success:
+	pla		; the dangling y push
+	lda #0		; success, zero flag
+	pha
+continue:
+	ldx _savex
+	pla
+	rts
+.endproc
+
 
 reset:
 	jsr library::io::outstr
