40f58245c13ef6bc36431c6fbcd8e15a3c6f0f17
Adrian Conlon <adrian.conlon@gmail.com>
2016-05-31 15:49:25 +0100

    "drop" can have a very simple shared implementation.
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- sudoku.s
+++ sudoku.s
@@ -6,8 +6,8 @@
 
 ; Clock cycles:
 ;
-; 65sc02	226,441,485	1 minute 53 seconds @ 2Mhz
-; 6502		307,640,819	2 minutes 33 seconds @ 2Mhz
+; 65sc02	219,472,305	1 minute 49 seconds @ 2Mhz
+; 6502		287,868,799	2 minutes 24 seconds @ 2Mhz
 
         .setcpu "6502"
 
